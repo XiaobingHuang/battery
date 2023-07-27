07c1783858eb9c209ba517c3cd2bca3462c0f708
xhuang35 <xhuang35@buffalo.edu>
2023-07-27 14:35:01 -0400

    left card

---
+++ src/app/views/batteryDetail/views/BatteryMonitor/components/LeftCard/index.tsx
@@ -0,0 +1,13 @@
+import { Tabs,Typography,Col, Row,Space, Card, Layout } from 'antd';
+import type { TabsProps } from 'antd';
+const { Title, Paragraph, Text, Link } = Typography;
+import {  } from 'antd';
+const  LeftCard=()=>{
+
+    return(
+        <Card title="Battery name">
+
+        </Card>
+    )
+}
+export default LeftCard;
